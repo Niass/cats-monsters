11cf815b6ce5a744efa890d5338a0faa7672c17d
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-09 05:29:15 +0200

    Updates

---
+++ precache-manifest.eb579285dbfa1553d909a9eb3c905824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed74259426885729d44154d299665fa8",
+    "url": "https//Niass.github.io/cats-monsters/index.html"
+  },
+  {
+    "revision": "0cc44b1a5058484349b9",
+    "url": "https//Niass.github.io/cats-monsters/static/css/main.16290f68.chunk.css"
+  },
+  {
+    "revision": "2cc0f24d0c53572ab72e",
+    "url": "https//Niass.github.io/cats-monsters/static/js/2.e0f5d2bd.chunk.js"
+  },
+  {
+    "revision": "0cc44b1a5058484349b9",
+    "url": "https//Niass.github.io/cats-monsters/static/js/main.fb9470a8.chunk.js"
+  },
+  {
+    "revision": "bd3db26e9f578bd397d3",
+    "url": "https//Niass.github.io/cats-monsters/static/js/runtime-main.5198a7f8.js"
+  }
+]);
